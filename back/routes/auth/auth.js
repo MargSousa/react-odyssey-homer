aab39b6a305e5757184f65f3b5d0373672b21de5
Margarida Sousa <margaridasousa@MBPdeMargarida2.lan>
2020-06-22 13:16:32 +0100

    quest040

--- back/routes/auth/auth.js
+++ back/routes/auth/auth.js
@@ -1,8 +1,17 @@
 const  express  =  require('express');
 const router = express.Router();
+const connection = require('../../helpers/db');
 
 router.post('/signup', function(req, res, next) {
-  res.send('I am in POST signup');
+  const formData = req.body;
+  console.log(formData);
+  connection.query('INSERT INTO users SET ?', [formData], (err, results) => {
+    if (err) {
+      res.status(500).send("Error saving new user").end();
+    } else {
+      res.sendStatus(200).end();
+    }
+  });
 });
 
 module.exports = router;
